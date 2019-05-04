7ef1e0111f4927b40a4b4c7286c06a78952a1ea0
blbst123 <billloukehao@gmail.com>
2019-05-03 17:45:03 -0700

    Implement login/register for app.js
    - Begin cookieParser middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,9 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  next();
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
-
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,9 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie !== undefined) {
+    console.log(req.headers.cookie.substring(10));
+  }
+  // req.cookies = {shortlyid: req.headers.cookie.substring(10)};
+  next();
 };
 
 module.exports = parseCookies;
